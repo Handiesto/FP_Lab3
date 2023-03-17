736817e5398beebf8f3f4c20c07cf890b3ad929b
Handiesto <klim.plyasov@gmail.com>
2023-03-17 05:18:07 +0300

    fix_test

--- test/approx.ml
+++ test/approx.ml
@@ -3,20 +3,20 @@ open! Approx
 
 let float_cmp a b = (a -. b) *. (a -. b) < 0.01
 
-let points =
-        [ (1., 2.); (3., 4.); (5., 6.); (7., 8.); (9., 10.); (11., 12.); (13., 14.) ]
+
 
 let test_linear _ =
+  let points = [ (1., 2.); (3., 4.); (5., 6.); (7., 8.); (9., 10.); (11., 12.); (13., 14.) ] in
   let f = Approx.linear points in
   OUnit2.assert_equal ~cmp:float_cmp 2. (f 1.);
   OUnit2.assert_equal ~cmp:float_cmp 5.5 (f 4.5);
   OUnit2.assert_equal ~cmp:float_cmp 6.7 (f 5.7)
 
 let test_segment _ =
+  let points = [(3., 4.); (2., 4.); (1., 1.)] in
   let f = Approx.segment points in
-  OUnit2.assert_equal ~cmp:float_cmp 10.0 (f 2.5);
-  OUnit2.assert_equal ~cmp:float_cmp 4.0 (f 1.023);
-  OUnit2.assert_equal ~cmp:float_cmp 6.0 (f 4.6)
+  OUnit2.assert_equal ~cmp:float_cmp 4. (f 2.5);
+  OUnit2.assert_equal ~cmp:float_cmp 2.5 (f 1.5)
 
 let test =
   "test" >::: ["test_linear" >:: test_linear; "test_segment" >:: test_segment]
