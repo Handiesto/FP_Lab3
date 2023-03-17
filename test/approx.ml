dc88d9283b71806ec660c56e4b6ea62737800f78
Handiesto <klim.plyasov@gmail.com>
2023-03-17 05:38:26 +0300

    tests

--- test/approx.ml
+++ test/approx.ml
@@ -24,7 +24,14 @@ let test_segment _ =
   OUnit2.assert_equal ~cmp:float_cmp 4. (f 2.5) ;
   OUnit2.assert_equal ~cmp:float_cmp 2.5 (f 1.5)
 
+let test_logarifm _ =
+  let points = [ (1., 2.); (3., 4.); (5., 6.); (7., 8.); (9., 10.); (11., 12.); (13., 14.) ] in
+  let f = Approx.logarifm points in
+  OUnit2.assert_equal ~cmp:float_cmp 0.4406432356126949 (f 1.02) ;
+  OUnit2.assert_equal ~cmp:float_cmp 4.503721892587115 (f 2.5) ;
+  OUnit2.assert_equal ~cmp:float_cmp 7.364782591706943 (f 4.7)
+
 let test =
-  "test" >::: ["test_linear" >:: test_linear; "test_segment" >:: test_segment]
+  "test" >::: ["test_linear" >:: test_linear; "test_segment" >:: test_segment; "test_logarifm" >:: test_logarifm]
 
 let () = run_test_tt_main test
